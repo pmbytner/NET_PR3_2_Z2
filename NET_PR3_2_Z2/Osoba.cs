595b9ee0e58dafe655ec81f018adf567793c5ede
PMBytner <pawel.m.bytner@gmail.com>
2023-02-25 18:40:48 +0100

    Dodaj pliki projektów.

---
+++ NET_PR3_2_Z2/Osoba.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_PR3_2_Z2;
+public class Osoba
+{
+	public string Imię { get; set; } = "Nemo";
+}
